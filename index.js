ca2ecb09beedfbce83e3f9ffbcbc6178950fd761
Rebeca Rello <rebeca.rello@gmail.com>
2023-02-06 12:33:05 +0100

    primer commit

---
+++ index.js
@@ -0,0 +1,49 @@
+function calculadoraSuma(op1, op2){
+    
+            return(op1 + op2);
+      
+    }
+
+  let resultadosuma=calculadoraSuma(6,3);
+  console.log(resultadosuma);
+  
+  
+  function calculadoraResta(op1, op2){
+    
+    return(op1 - op2);
+
+}
+
+
+
+let resultadoresta=calculadoraResta(6,3);
+console.log(resultadoresta);
+
+function calculadoraMultiplicacion(op1, op2){
+    
+        return(op1 * op2);
+    
+    }
+    
+    let resultadomulti=calculadoraMultiplicacion(6,3);
+    console.log(resultadomulti);
+
+function calculadoraDivision(op1, op2){
+    
+    return(op1 / op2);
+
+}
+
+let resultadodivi=calculadoraDivision(6,3);
+console.log(resultadodivi);
+
+module.exports ={calculadoraSuma,calculadoraResta, calculadoraMultiplicacion, calculadoraDivision };
+
+
+
+function cuadrado(op1) {
+    
+    return op1*op1
+
+}
+
